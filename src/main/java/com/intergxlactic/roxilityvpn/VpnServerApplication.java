a50868ad90257a1a723f8035a1116b77a6814018
reefer madnes <immunity.heart123@gmail.com>
2024-07-21 11:43:58 +0300

    add dependencies

--- src/main/java/com/intergxlactic/roxilityvpn/VpnServerApplication.java
+++ src/main/java/com/intergxlactic/roxilityvpn/VpnServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoxilityVpnApplication {
+public class VpnServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RoxilityVpnApplication.class, args);
+        SpringApplication.run(VpnServerApplication.class, args);
     }
 
 }
