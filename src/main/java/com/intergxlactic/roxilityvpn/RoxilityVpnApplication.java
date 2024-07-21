dac108b6d32ec0dac7d59eaac9070133681cecab
reefer madnes <immunity.heart123@gmail.com>
2024-07-21 11:30:00 +0300

    create project

---
+++ src/main/java/com/intergxlactic/roxilityvpn/RoxilityVpnApplication.java
@@ -0,0 +1,13 @@
+package com.intergxlactic.roxilityvpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoxilityVpnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoxilityVpnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intergxlactic/roxilityvpn/RoxilityVpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intergxlactic.roxilityvpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoxilityVpnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
